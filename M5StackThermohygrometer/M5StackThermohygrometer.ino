3de55b01ea7dd0a854975450a5f3b720050fe0cd
chuke <ken.0416.leo@gmail.com>
2022-01-26 07:27:42 +0900

    support button events in main

--- M5StackThermohygrometer/M5StackThermohygrometer.ino
+++ M5StackThermohygrometer/M5StackThermohygrometer.ino
@@ -1,6 +1,9 @@
 #include <M5Stack.h>
 #include <WiFi.h>
+#include <stdexcept>
+#include "ButtonInputWatcher.hpp"
 #include "EnvironmentDataCollector.hpp"
+#include "EventHandler.hpp"
 #include "NetworkController.hpp"
 #include "SDCardController.hpp"
 
@@ -14,16 +17,27 @@ void setup()
     M5.Lcd.setTextSize(2);
     Wire.begin();
 
-    sNetworkSettings settings = SDCardController::ReadNetworkSettings();
-    char* ssid = const_cast<char*>(settings.ssid.c_str());
-    char* password = const_cast<char *>(settings.password.c_str());
-    NetworkController* network_controller = new NetworkController(ssid, password);
-    bool result = network_controller->Prepare();
-    M5.Lcd.printf("NetworkController::Prepare() result = %d\n", result);
+    try 
+    {
+        sNetworkSettings settings = SDCardController::ReadNetworkSettings();
+        char* ssid = const_cast<char*>(settings.ssid.c_str());
+        char* password = const_cast<char *>(settings.password.c_str());
+        NetworkController* network_controller = new NetworkController(ssid, password);
+        bool result = network_controller->Prepare();
+        M5.Lcd.printf("NetworkController::Prepare() result = %d\n", result);
+    }
+    catch (std::invalid_argument e)
+    {
+        Serial.println(e.what());
+    }
 
     CreateTasks();
+
+    ButtonInputWatcher::GetInstance()->SetUpButtonInterruption();
 }
 
 void loop()
 {
+    EventHandler::GetInstance()->EventHandle();
+    delay(10);
 }
