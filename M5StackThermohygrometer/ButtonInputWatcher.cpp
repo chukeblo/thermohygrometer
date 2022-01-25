cf4b3917e8883caacb2d87bb55dfff252628149f
chuke <ken.0416.leo@gmail.com>
2022-01-26 07:03:41 +0900

    disable left button due to issue with its simultaneous use with SD card

--- M5StackThermohygrometer/ButtonInputWatcher.cpp
+++ M5StackThermohygrometer/ButtonInputWatcher.cpp
@@ -3,9 +3,16 @@
 #include <M5Stack.h>
 #include "EventHandler.hpp"
 
-static const uint8_t kLeftButtonPin = 39;
+// static const uint8_t kLeftButtonPin = 39;
 static const uint8_t kMiddleButtonPin = 38;
 static const uint8_t kRightButtonPin = 37;
+
+ButtonInputWatcher::ButtonInputWatcher()
+{
+	// left_button_pressed_ = false;
+	middle_button_pressed_ = false;
+	right_button_pressed_ = false;
+}
 
 ButtonInputWatcher::~ButtonInputWatcher()
 {
@@ -23,31 +30,32 @@ ButtonInputWatcher* ButtonInputWatcher::GetInstance()
 
 void ButtonInputWatcher::SetUpButtonInterruption()
 {
-	pinMode(kLeftButtonPin, INPUT);
+	// pinMode(kLeftButtonPin, INPUT);
 	pinMode(kMiddleButtonPin, INPUT);
 	pinMode(kRightButtonPin, INPUT);
 
-	attachInterrupt(kLeftButtonPin, OnLeftButtonPressed, FALLING);
+	// attachInterrupt(kLeftButtonPin, OnLeftButtonPressed, FALLING);
 	attachInterrupt(kMiddleButtonPin, OnMiddleButtonPressed, FALLING);
 	attachInterrupt(kRightButtonPin, OnRightButtonPressed, FALLING);
 }
 
 void ButtonInputWatcher::OnButtonPressed(ButtonType type)
 {
-	bool isPressed = false;
 	switch (type)
 	{
-	case ButtonType::kLeftButton:
-		if (!left_button_pressed_) {
-			left_button_pressed_ = true;
-			EventHandler::GetInstance()->AddEvent(EventType::kLeftButtonPressed);
-			left_button_pressed_ = false;
-		}
-		break;
+	// case ButtonType::kLeftButton:
+	// 	if (!left_button_pressed_) {
+	// 		left_button_pressed_ = true;
+	// 		EventHandler::GetInstance()->AddEvent(EventType::kLeftButtonPressed);
+	// 		Serial.println("Left Button Pressed.");
+	// 		left_button_pressed_ = false;
+	// 	}
+	// 	break;
 	case ButtonType::kMiddleButton:
 		if (!middle_button_pressed_) {
 			middle_button_pressed_ = true;
 			EventHandler::GetInstance()->AddEvent(EventType::kMiddleButtonPressed);
+			Serial.println("Middle Button Pressed.");
 			middle_button_pressed_ = false;
 		}
 		break;
@@ -55,6 +63,7 @@ void ButtonInputWatcher::OnButtonPressed(ButtonType type)
 		if (!right_button_pressed_) {
 			right_button_pressed_ = true;
 			EventHandler::GetInstance()->AddEvent(EventType::kRightButtonPressed);
+			Serial.println("Right Button Pressed.");
 			right_button_pressed_ = false;
 		}
 		break;
@@ -63,10 +72,10 @@ void ButtonInputWatcher::OnButtonPressed(ButtonType type)
 	}
 }
 
-void OnLeftButtonPressed()
-{
-	ButtonInputWatcher::GetInstance()->OnButtonPressed(ButtonType::kLeftButton);
-}
+// void OnLeftButtonPressed()
+// {
+// 	ButtonInputWatcher::GetInstance()->OnButtonPressed(ButtonType::kLeftButton);
+// }
 
 void OnMiddleButtonPressed()
 {
--- M5StackThermohygrometer/ButtonInputWatcher.hpp
+++ M5StackThermohygrometer/ButtonInputWatcher.hpp
@@ -2,6 +2,8 @@
 
 #include "ButtonType.hpp"
 
+// SDカードを使用すると左ボタンが押されっぱなしの判定になってしまうため、
+// 解決方法が見つかるまで左ボタンは使用しないこととする。
 class ButtonInputWatcher
 {
 public:
@@ -13,11 +15,11 @@ private:
 	ButtonInputWatcher();
 	~ButtonInputWatcher();
 
-	bool left_button_pressed_;
+	// bool left_button_pressed_;
 	bool middle_button_pressed_;
 	bool right_button_pressed_;
 };
 
-void OnLeftButtonPressed();
+// void OnLeftButtonPressed();
 void OnMiddleButtonPressed();
 void OnRightButtonPressed();
