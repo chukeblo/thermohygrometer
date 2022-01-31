084ed510082734177c1dac68ee5a57eaf85001bb
chuke <ken.0416.leo@gmail.com>
2022-02-01 00:23:29 +0900

    delete unused ctor in event data

--- M5StackThermohygrometer/EventData.hpp
+++ M5StackThermohygrometer/EventData.hpp
@@ -5,10 +5,4 @@
 typedef struct EventData {
     EventType type;
     void* data;
-
-    EventData(EventType event_type, void* event_data)
-    {
-        type = event_type;
-        data = event_data;
-    }
 } sEventData;
