965cb159c107fea19848a26b4c8d467b336a7e15
chuke <ken.0416.leo@gmail.com>
2022-02-23 22:22:54 +0900

    rewrite setContentView by viewBinding

--- AndroidThermohygrometer/app/src/main/java/com/example/androidthermohygrometer/MainActivity.java
+++ AndroidThermohygrometer/app/src/main/java/com/example/androidthermohygrometer/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
