adbe51b16bcbbfc1b279ff5bf112479a9cd9ba64
chuke <ken.0416.leo@gmail.com>
2022-03-10 02:22:30 +0900

    add log for measurement result

--- M5StackThermohygrometer/ThermohygroDataMeasurer.cpp
+++ M5StackThermohygrometer/ThermohygroDataMeasurer.cpp
@@ -1,6 +1,8 @@
 #include "ThermohygroDataMeasurer.hpp"
 
+#include "ConsoleLogger.hpp"
 #include "EventHandler.hpp"
+#include "LogConstants.hpp"
 #include "MeasurementResult.hpp"
 
 ThermohygroDataMeasurer::ThermohygroDataMeasurer()
@@ -27,6 +29,11 @@ void ThermohygroDataMeasurer::ReadThermohygroData()
 			if (data)
 			{
 				MeasurementResult* result = new MeasurementResult(GetStringTimeFrom(&tm), data);
+				ConsoleLogger::Log(new LogData(LogLevel::kDebug, kThermohygroDataMeasurer, kReadThermohygroData,
+					"measurement result: time=" + result->time +
+					", temperature=" + std::string(String(result->thermohygro_data->temperature).c_str()) +
+					", humidity=" + std::string(String(result->thermohygro_data->humidity).c_str())
+				));
 				EventHandler::GetInstance()->AddEvent(new EventData(EventType::kReadEnvData, (void*)result));
 			}
 		}
