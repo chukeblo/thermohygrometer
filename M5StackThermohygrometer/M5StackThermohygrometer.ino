718c13ce0819673a6b64b07362b4ff438515739e
chuke <ken.0416.leo@gmail.com>
2022-03-14 23:02:55 +0900

    add creating task function in main setup

--- M5StackThermohygrometer/M5StackThermohygrometer.ino
+++ M5StackThermohygrometer/M5StackThermohygrometer.ino
@@ -2,9 +2,14 @@
 #include <WiFi.h>
 #include "EventHandler.hpp"
 #include "GUIManager.hpp"
+#include "TaskManager.hpp"
+#include "ThermohygroDataMeasurer.hpp"
+
+const char* kMeasureTask = "MeasureTask";
 
 EventHandler* event_handler = EventHandler::GetInstance();
 GUIManager* gui_manager = new GUIManager();
+TaskManager* task_manager = TaskManager::GetInstance();
 
 void setup()
 {
@@ -13,6 +18,8 @@ void setup()
 
     event_handler->Initialize();
     event_handler->SetGUIManager(gui_manager);
+    ThermohygroDataMeasurer* thermohygrodata_measurer = new ThermohygroDataMeasurer();
+    task_manager->CreateTask(kMeasureTask, (void*)thermohygrodata_measurer);
 }
 
 void loop()
