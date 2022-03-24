e565a2a1c434bfc29ed0925619eca486ba43fbe0
chuke <ken.0416.leo@gmail.com>
2022-03-25 00:18:12 +0900

    make properties immutable in aws communication resources

--- AndroidThermohygrometer/app/src/main/java/com/example/androidthermohygrometer/models/AwsConnectionResources.java
+++ AndroidThermohygrometer/app/src/main/java/com/example/androidthermohygrometer/models/AwsConnectionResources.java
@@ -1,12 +1,12 @@
 package com.example.androidthermohygrometer.models;
 
 public class AwsConnectionResources {
-    private String clientId;
-    private String endpoint;
-    private String topic;
-    private String rootCa;
-    private String deviceCert;
-    private String privateKey;
+    private final String clientId;
+    private final String endpoint;
+    private final String topic;
+    private final String rootCa;
+    private final String deviceCert;
+    private final String privateKey;
 
     public AwsConnectionResources(String clientId, String endpoint, String topic, String rootCa, String deviceCert, String privateKey) {
         this.clientId = clientId;
@@ -21,47 +21,23 @@ public class AwsConnectionResources {
         return this.clientId;
     }
 
-    public void setClientId(String clientId) {
-        this.clientId = clientId;
-    }
-
     public String getEndpoint() {
         return this.endpoint;
-    }
-
-    public void setEndpoint(String endpoint) {
-        this.endpoint = endpoint;
     }
 
     public String getTopic() {
         return this.topic;
     }
 
-    public void setTopic(String topic) {
-        this.topic = topic;
-    }
-
     public String getRootCa() {
         return this.rootCa;
-    }
-
-    public void setRootCa(String rootCa) {
-        this.rootCa = rootCa;
     }
 
     public String getDeviceCert() {
         return this.deviceCert;
     }
 
-    public void setDeviceCert(String deviceCert) {
-        this.deviceCert = deviceCert;
-    }
-
     public String getPrivateKey() {
         return this.privateKey;
-    }
-
-    public void setPrivateKey(String privateKey) {
-        this.privateKey = privateKey;
     }
 }
