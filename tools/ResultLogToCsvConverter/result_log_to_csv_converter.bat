af316c8eb92a78b1960de3782b591cb182f883da
chuke <ken.0416.leo@gmail.com>
2022-03-29 23:37:54 +0900

    modify pathes due to adding src directory

--- tools/ResultLogToCsvConverter/result_log_to_csv_converter.bat
+++ tools/ResultLogToCsvConverter/result_log_to_csv_converter.bat
@@ -1,3 +1,3 @@
 @echo off
-gcc -Wall -o result_log_to_csv_converter.exe main.c convert_to_csv.c
+gcc -Wall -o result_log_to_csv_converter.exe src/main.c src/convert_to_csv.c
 result_log_to_csv_converter.exe %1 %2
